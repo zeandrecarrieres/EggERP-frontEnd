91992cbddc2df7665e4b5f5b708832ba3601c68b
Zé Carrières <zeandredev@gmail.com>
2021-05-02 19:44:31 +0200

    urls

--- src/pages/AddTransaction.jsx
+++ src/pages/AddTransaction.jsx
@@ -20,7 +20,7 @@ function AddTransaction() {
     e.preventDefault();
     
     //await fetch("http://localhost:3001/transaction", {
-      wait fetch("https://nutriovosapp-backend.herokuapp.com/transaction", {
+      await fetch("https://nutriovosapp-backend.herokuapp.com/transaction", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
